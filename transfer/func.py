e151eb189290efe62a192e04c750099eb2c6a160
conner <gorman.connerm@gmail.com>
2023-04-30 00:04:40 -0400

    lotsa logging

--- transfer/func.py
+++ transfer/func.py
@@ -3,6 +3,7 @@ import os
 import json
 import sys
 import base64
+import logging
 
 from fdk import response
 import oci.object_storage
@@ -19,9 +20,16 @@ def handler(ctx, data: io.BytesIO=None):
         raise Exception(body)
     get_resp = get_object(bucket_name, object_name)
     put_resp = put_object("receive-from-oci", object_name, get_resp["content"])
+    if put_resp == False:
+        logging.getLogger().error("Upload failed")
+        return response.Response(
+            ctx,
+            response_data="Upload failed",
+            headers={"Content-Type": "application/json"}
+        )
     return response.Response(
         ctx,
-        response_data=json.dumps(put_resp),
+        response_data="Upload succssful",
         headers={"Content-Type": "application/json"}
     )
 
@@ -33,6 +41,7 @@ def get_object(bucket_name, object_name):
         print("Searching for bucket and object", flush=True)
         object = client.get_object(namespace, bucket_name, object_name)
         print("found object", flush=True)
+        logging.getLogger().info("Found object.")
         if object.status == 200:
             print("Success: The object " + object_name + " was retrieved with the content: " + object.data.text, flush=True)
             message = object.data.text
@@ -45,23 +54,34 @@ def get_object(bucket_name, object_name):
 
 def put_object(bucket_name, object_name, content):
     # Get AWS secret
+    logging.getLogger().info("Starting AWS get creds")
     signer = oci.auth.signers.InstancePrincipalsSecurityTokenSigner()
     client = oci.secrets.SecretsClient(config={}, signer=signer)
-    response = client.get_secret_bundle(secret_id="ocid1.vaultsecret.oc1.iad.amaaaaaapc6swyaavfxklakk4cgkbii4grau27umnsmxoos5y4t5psqljbba")
+    try:
+        response = client.get_secret_bundle(secret_id="ocid1.vaultsecret.oc1.iad.amaaaaaapc6swyaavfxklakk4cgkbii4grau27umnsmxoos5y4t5psqljbba")
+        logging.getLogger().info("Got secret from vault")
+    except Exception as e:
+        raise Exception("Failed to get secret from Vault")
     b64_secret = response.data.secret_bundle_content.content
     secret = base64.b64decode(b64_secret).decode('utf-8')
 
-    
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id='AKIAUMKYHCUXXPUXVX5S',
-        aws_secret_access_key=secret,
-        aws_default_region='us-ashburn-1'
-    )
+    logging.getLogger().info("Get aws client")
+    try:
+        s3_client = boto3.client(
+            's3',
+            aws_access_key_id='AKIAUMKYHCUXXPUXVX5S',
+            aws_secret_access_key=secret,
+            aws_default_region='us-ashburn-1'
+        )
+    except Exception as e:
+        logging.getLogger().error("AWS client failed")
+        raise Exception("AWS client failed") from e
 
         
     try:
         response = s3_client.put_object(Body=bytes(content), Bucket=bucket_name, Key=object_name)
+        logging.getLogger().info("Put successful")
     except boto3.ClientError as e:
+        logging.getLogger().error("AWS s3 failed")
         return False
     return True
